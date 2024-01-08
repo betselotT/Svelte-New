029e7af42f85fee4b32449e5027061756c22e08a
Betselot Tesfa <betselottesfa9@gmail.com>
2024-01-07 23:25:21 -0900

    new

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  let count = 0;
+  function increment() {
+    count++;
+  }
+  function decrement() {
+    count--;
+  }
+</script>
+
+{count}
+
+<button on:click={increment}> Count Up! </button>
+<button on:click={decrement}> Count Down! </button>
